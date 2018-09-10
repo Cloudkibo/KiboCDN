bbaacf6365b669a7634fb255095c2074b0800e09
Sojharo <sojharo@live.com>
2018-09-10 10:59:38 +0500

    work on ssl #3

--- server.js
+++ server.js
@@ -1,65 +1,31 @@
 const http = require('http')
-const url = require('url')
+const https = require('https')
 const fs = require('fs')
-const path = require('path')
 // you can pass the parameter in the command line. e.g. node static_server.js 3000
 const port = process.argv[2] || 9000
-// maps file extention to MIME types
-const mimeType = {
-  '.ico': 'image/x-icon',
-  '.html': 'text/html',
-  '.js': 'text/javascript',
-  '.json': 'application/json',
-  '.css': 'text/css',
-  '.png': 'image/png',
-  '.jpg': 'image/jpeg',
-  '.wav': 'audio/wav',
-  '.mp3': 'audio/mpeg',
-  '.svg': 'image/svg+xml',
-  '.pdf': 'application/pdf',
-  '.doc': 'application/msword',
-  '.eot': 'appliaction/vnd.ms-fontobject',
-  '.ttf': 'aplication/font-sfnt'
+const securePort = process.argv[3] || 8443
+const env = process.env.NODE_ENV || 'development'
+
+let options = {
+  ca: '',
+  key: '',
+  cert: ''
 }
-http.createServer((req, res) => {
-  console.log(`${req.method} ${req.url}`)
-  // parse URL
-  const parsedUrl = url.parse(req.url)
-  // extract URL path
-  // Avoid https://en.wikipedia.org/wiki/Directory_traversal_attack
-  // e.g curl --path-as-is http://localhost:9000/../fileInDanger.txt
-  // by limiting the path to current directory only
-  /* eslint-disable */
-  const sanitizePath = path.normalize(parsedUrl.pathname).replace(/^(\.\.[\/\\])+/, '')
-  /* eslint-enable */
-  let pathname = path.join(__dirname, sanitizePath)
-  fs.access(pathname, fs.constants.F_OK, (err) => {
-    if (err) {
-      // if the file is not found, return 404
-      res.statusCode = 404
-      res.end(`File ${req.url} not found!`)
-      return
+
+if (env === 'production') {
+  try {
+    options = {
+      ca: fs.readFileSync('/root/certs/kibopush.ca-bundle'),
+      key: fs.readFileSync('/root/certs/kibopush.key'),
+      cert: fs.readFileSync('/root/certs/kibopush.crt')
     }
-    // if is a directory, then look for index.html
-    if (fs.statSync(pathname).isDirectory()) {
-      // pathname += '/index.html'
-      res.statusCode = 404
-      res.end(`Please provide file name as well.`)
-      return
-    }
-    // read file from file system
-    fs.readFile(pathname, (err, data) => {
-      if (err) {
-        res.statusCode = 500
-        res.end(`Error getting the file: ${err}.`)
-      } else {
-        // based on the URL path, extract the file extention. e.g. .js, .doc, ...
-        const ext = path.parse(pathname).ext
-        // if the file is found, set Content-type and send data
-        res.setHeader('Content-type', mimeType[ext] || 'text/plain')
-        res.end(data)
-      }
-    })
-  })
-}).listen(parseInt(port))
-console.log(`Server listening on port ${port}`)
+  } catch (e) {
+    console.log('Error in loading certs')
+    console.log(e)
+  }
+}
+
+http.createServer(require('./logic').logic).listen(parseInt(port))
+https.createServer(options, require('./logic').logic).listen(parseInt(securePort))
+
+console.log(`Server listening on ports ${port} and ${securePort}`)
