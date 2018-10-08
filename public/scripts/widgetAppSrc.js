b9d6ba07c8a38da1c2330164b4e068d1c4a043b8
Imran Shoukat <imran.shoukat1995@gmail.com>
2018-10-08 14:52:19 +0500

    Update widgetAppSrc.js

--- public/scripts/widgetAppSrc.js
+++ public/scripts/widgetAppSrc.js
@@ -99,7 +99,7 @@
     infoPayload.pageTitle = document.title;
 
     var xmlhttp = new XMLHttpRequest();
-    var url = "https://app.kibopush.com/api/ip2country/findIp";
+    var url = "https://kibopush-imran.ngrok.io/api/ip2country/findIp";
 
     xmlhttp.onreadystatechange = function() {
       if (this.readyState === 4 && this.status === 200) {
