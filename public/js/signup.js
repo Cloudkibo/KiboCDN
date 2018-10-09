5900639f2563b1197a2f65d680a117e61b6d866f
Sojharo <sojharo@live.com>
2018-10-09 15:43:50 +0500

    work on signup #42

--- public/js/signup.js
+++ public/js/signup.js
@@ -3,6 +3,13 @@
  */
 var queryString = window.location.href.split('?')[1]
 
+var tokenCookie = readCookie("token")
+
+if (tokenCookie) {
+    // TODO CORRECT THIS TO NEW URLs WHEN AVAILABLE
+    window.location.replace('https://app.kibopush.com';
+}
+
 $(document).ready(function() {
   $(".applyBtn").click(function () {
     document.getElementById("alertMsg").innerHTML = ""
@@ -51,3 +58,14 @@ $(document).ready(function() {
     });
   })
 })
+
+function readCookie(name) {
+    var nameEQ = name + "=";
+    var ca = document.cookie.split(';');
+    for(var i=0;i < ca.length;i++) {
+      var c = ca[i];
+      while (c.charAt(0)==' ') c = c.substring(1,c.length);
+      if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
+    }
+    return null;
+  }
