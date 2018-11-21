c0440b49dbfb0ae0eea36f9ffea6a0d6d2ebd20b
Sojharo <sojharo@live.com>
2018-11-21 16:02:26 +0500

    work on redirect #12

--- public/js/signin.js
+++ public/js/signin.js
@@ -5,10 +5,6 @@ var queryString = window.location.href.split('?')[1]
 var environment = window.location.href.split('.')[0].split('//')[1] === 'saccounts' ? 'staging' : 'production';
 var tokenCookie = readCookie("token")
 
-var stagingUrl = 'https://saccounts.cloudkibo.com/?continue=https://skiboengage.cloudkibo.com';
-var productionUrl = 'https://accounts.cloudkibo.com/?continue=https://kiboengage.cloudkibo.com';
-var defaultURL = (environment === 'staging') ? stagingUrl : productionUrl
-
 $(document).ready(function() {
   $(".loginBtn").click(function () {
     document.getElementById("alertMsg").innerHTML = ""
--- public/js/signup.js
+++ public/js/signup.js
@@ -5,26 +5,6 @@ var queryString = window.location.href.split('?')[1]
 var environment = window.location.href.split('.')[0].split('//')[1] === 'saccounts' ? 'staging' : 'production';
 var tokenCookie = readCookie("token")
 
-var stagingUrl = 'https://saccounts.cloudkibo.com/?continue=https://skiboengage.cloudkibo.com';
-var productionUrl = 'https://accounts.cloudkibo.com/?continue=https://kiboengage.cloudkibo.com';
-var defaultURL = (environment === 'staging') ? stagingUrl : productionUrl
-
-if (queryString === undefined) {
-  if (tokenCookie) redirectTheLoggedIn(defaultURL)
-  else window.location.replace(defaultURL)
-} else if (queryString.split('=')[1] === undefined) {
-  if (tokenCookie) redirectTheLoggedIn(defaultURL)
-  else window.location.replace(defaultURL)
-} else {
-  if (tokenCookie) redirectTheLoggedIn(queryString)
-}
-
-function redirectTheLoggedIn (redirectURL) {
-  var finalRedirectURL = redirectURL.split('=')[1];
-  console.log('final redirect URL : ', finalRedirectURL);
-  window.location.replace(finalRedirectURL);
-}
-
 $(document).ready(function() {
   $(".signUpBtn").click(function () {
     document.getElementById("alertMsg").innerHTML = ""
