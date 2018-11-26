352d168473afa8413daca1ba37adae517d025958
arveen kumar <arveenkumar55@gmail.com>
2018-11-26 13:41:19 +0500

    logs

--- public/js/signup.js
+++ public/js/signup.js
@@ -4,7 +4,11 @@
 var queryString = window.location.href.split('?')[1]
 var environment = window.location.href.split('.')[0].split('//')[1] === 'saccounts' ? 'staging' : 'production';
 var tokenCookie = readCookie("token")
-
+function validateEmail(email) 
+{
+  var re = /^(?:[a-z0-9!#$%&amp;'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&amp;'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])$/;
+  return re.test(email)
+}
 $(document).ready(function() {
   $(".signUpBtn").click(function () {
     document.getElementById("alertMsg").innerHTML = ""
@@ -24,6 +28,12 @@ $(document).ready(function() {
       return document.getElementById(
         "alertMsg").innerHTML = "please Enter email"
     }
+    if(!validateEmail(email)) {
+      return document.getElementById(
+        "alertMsg").innerHTML = "please Enter valid email"
+    }
+
+    
     if(password.length == 0) {
       return document.getElementById(
         "alertMsg").innerHTML = "please Enter valid Password"
