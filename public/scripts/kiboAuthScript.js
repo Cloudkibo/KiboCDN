83be5a9eedf9e0a72b4c9df9b38fd146f00d8ae3
Sojharo <sojharo@live.com>
2019-04-30 15:28:42 +0500

    final work on CloudKibo/KiboPush#5253

--- public/scripts/kiboAuthScript.js
+++ public/scripts/kiboAuthScript.js
@@ -30,7 +30,7 @@ function redirectToLoginAccounts() {
   const environment = readCookie('environment') || window.__kibo_environment
   const url_production = readCookie('url_production') || window.__url_production
   const url_staging = readCookie('url_staging') || window.__url_staging
-  const url_development = readCookie('url_development') || window.__url_development
+  const url_development = window.__url_development || readCookie('url_development')
   if (environment === 'development') window.location.replace('http://localhost:3024/?continue=' + url_development)
   if (environment === 'staging') window.location.replace('https://saccounts.cloudkibo.com/?continue=' + url_staging)
   if (environment === 'production') window.location.replace('https://accounts.cloudkibo.com/?continue=' + url_production)
